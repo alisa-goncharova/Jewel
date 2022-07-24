df7bde4f448d0b180474f207917ea8f4b1f60abc
Alisa Goncharova <61043716+alisa-goncharova@users.noreply.github.com>
2022-07-24 20:47:01 +0300

    feat: Добавления стартовой страницы

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="sidebar">
+        <div class="sidebar__close"></div>
+        <div class="sidebar__logo"></div>
+        <div class="sidebar__items">
+            <div class="sidebar__item">Валюты</div>
+            <div class="sidebar__item">Погода</div>
+        <!-- <div class="sidebar__item">Золото</div>-->
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.sidebar {
+    width: 370px;
+    height: 100vh;
+    padding: 30px;
+    box-shadow: 1px 4px 15px 1px rgba(30, 31, 38, 0.25);
+}
+.sidebar__close {}
+.sidebar__logo {
+    width: 300px;
+    height: 100px;
+    position: relative;
+    margin: 0 0 50px 0;
+}
+.sidebar__logo:before {
+    position: absolute;
+    content: '';
+    background-image: url(../components/icons/logo.png);
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+    background-size: 80%;
+}
+.sidebar__items {
+    text-align: center;
+}
+.sidebar__item {
+    margin: 0 0 25px 0;
+    color: #D0E1F9;
+    font-family: 'Roboto', sans-serif;
+    font-size: 22px;
+    font-weight: 400;
+    cursor: pointer;
+}
+.sidebar__item:last-child {
+    margin: 0 0 0 0;
+}
+</style>
---
+++ src/views/СurrenciesView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>This is currencies page</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: #D0E1F9;
+    font-size: 36px;
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+}
+</style>
